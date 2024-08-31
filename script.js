23d7b44224b790aa36bfd401ce5f662d49113a4d
Boxy <nozakisola@gmail.com>
2024-08-31 17:12:35 -0500

    Get Better

--- script.js
+++ script.js
@@ -1,73 +1,98 @@
-    const searchInput = document.getElementById('searchInput');
-    const searchMenu = document.getElementById('searchMenu');
-    const useHTTP = document.getElementById('usehttp');
-    const shortcuts = document.getElementById('shortcuts');
+const searchInput = document.getElementById("searchInput");
+const searchMenu = document.getElementById("SearchEngine");
+const shortcuts = document.getElementById("shortcuts");
 
-    var ShortcutsList = [];
+var ShortcutsList = [];
 
+searchInput.addEventListener("contextmenu", function (event) {
+  event.preventDefault();
+  searchMenu.style.display = "flex";
+  searchMenu.style.left = event.pageX + "px";
+  searchMenu.style.top = event.pageY + "px";
+});
 
-    searchInput.addEventListener('contextmenu', function(event) {
-      event.preventDefault();
-      searchMenu.style.display = 'flex';
-      searchMenu.style.left = event.pageX + 'px';
-      searchMenu.style.top = event.pageY + 'px';
-    });
+function searchGoogle() {
+  const query = searchInput.value;
+  window.open(
+    "https://www.google.com/search?q=" + encodeURIComponent(query),
+    "_blank"
+  );
+}
 
-    function searchGoogle() {
-      const query = searchInput.value;
-      window.open('https://www.google.com/search?q=' + encodeURIComponent(query), '_blank');
-    }
+function searchScholar() {
+  const query = searchInput.value;
+  window.open(
+    "https://scholar.google.com/scholar?q=" +
+      encodeURIComponent(query) +
+      "&bntG=",
+    "_blank"
+  );
+}
 
-    function searchScholar() {
-       const query = searchInput.value;
-       window.open('https://scholar.google.com/scholar?q=' + encodeURIComponent(query) + '&bntG=', '_blank');
-    }
+function searchBing() {
+  const query = searchInput.value;
+  window.open(
+    "https://www.bing.com/search?q=" + encodeURIComponent(query),
+    "_blank"
+  );
+}
 
-    function searchBing() {
-      const query = searchInput.value;
-      window.open('https://www.bing.com/search?q=' + encodeURIComponent(query), '_blank');
-    }
+function searchYahoo() {
+  const query = searchInput.value;
+  window.open(
+    "https://search.yahoo.com/search?p=" + encodeURIComponent(query),
+    "_blank"
+  );
+}
 
-    function searchYahoo() {
-      const query = searchInput.value;
-      window.open('https://search.yahoo.com/search?p=' + encodeURIComponent(query), '_blank');
-    }
+function searchNASA() {
+  const query = searchInput.value;
+  window.open(
+    "https://nasa.gov/?search=" + encodeURIComponent(query),
+    "_blank"
+  );
+}
 
-    function searchNASA() {
-      const query = searchInput.value;
-      window.open('https://nasa.gov/?search=' + encodeURIComponent(query), '_blank');
-    }
+function searchWiki() {
+  const query = searchInput.value;
+  window.open(
+    "https://en.wikipedia.org/w/index.php?search=" + encodeURIComponent(query),
+    "_blank"
+  );
+}
 
-    function searchWiki() {
-      const query = searchInput.value;
-      window.open('https://en.wikipedia.org/w/index.php?search=' + encodeURIComponent(query), '_blank');
-    }
+function searchSpace() {
+  const query = searchInput.value;
+  window.open(
+    "https://www.space.com/search?searchTerm=" + encodeURIComponent(query),
+    "_blank"
+  );
+}
 
-    function searchSpace() {
-      const query = searchInput.value;
-      window.open('https://www.space.com/search?searchTerm=' + encodeURIComponent(query), '_blank');
-    }
+function searchDuck() {
+  const query = searchInput.value;
+  window.open(
+    "https://duckduckgo.com/?q=" + encodeURIComponent(query),
+    "_blank"
+  );
+}
 
-    function goToURL() {
-      const query = searchInput.value;
-      if (useHTTP.checked) {
-        window.open("http://" + query, '_blank');
-      } else {
-        window.open("https://" + query, '_blank');
-      }
-    }
+function goToURL() {
+  const query = searchInput.value;
+  window.open("http://" + query, "_blank");
+}
 
-    searchInput.addEventListener('keypress', function(event) {
-  if (event.key === 'Enter') {
-    const query = searchInput.value;
-    window.open('https://www.google.com/search?q=' + encodeURIComponent(query), '_blank');
-  }});
+searchInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    search();
+  }
+});
 
 function newShortcut() {
-  let name = prompt("Shortcut Name")
-  let url = prompt("Shortcut URL")
-  const newShortcutButton = document.createElement('button');
-  const a = document.createElement('a');
+  let name = prompt("Shortcut Name");
+  let url = prompt("Shortcut URL");
+  const newShortcutButton = document.createElement("button");
+  const a = document.createElement("a");
   a.href = url;
   a.target = "_blank";
   a.rel = "noopener noreferrer";
@@ -75,6 +100,24 @@ function newShortcut() {
   a.appendChild(newShortcutButton);
   shortcuts.appendChild(a);
   //updateShortcuts()
+}
+
+function search() {
+  console.log(searchMenu.value);
+  switch (searchMenu.value) {
+    case "google":
+      searchGoogle();
+    case "yahoo":
+      searchYahoo();
+    case "bing":
+      searchBing();
+    case "duck":
+      searchDuck();
+    case "wiki":
+      searchWiki();
+    case "http":
+      goToURL();
+  }
 }
 
 /*if (localStorage.getItem("shortcuts") == null) {
